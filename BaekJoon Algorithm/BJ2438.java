631a51ed0c949516329e8ba33a00e50f907fabc8
jeon9825 <jeon9825@naver.com>
2019-03-08 09:28:20 +0900

    Add BJ2438.java

---
+++ BaekJoon Algorithm/BJ2438.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class BJ2438 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < i + 1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
