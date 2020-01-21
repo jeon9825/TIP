30d0a59fefe7008f6aaa6dba95fb177e126b420a
jeon9825 <jeon9825@naver.com>
2020-01-21 20:52:53 +0900

    Modify BJ9375.java //runtime error

--- BaekJoon Algorithm/BJ9375.java
+++ BaekJoon Algorithm/BJ9375.java
@@ -4,36 +4,34 @@ import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class BJ9375 {
 
-	public static int count(int t) {
-		Map<String, Integer> m = new HashMap<>();
-		Scanner scan = new Scanner(System.in);
-
-		for (int i = 0; i < t; i++) {
-			String s = scan.nextLine();
-			String[] sarr = s.split(" ");
-			if (!m.containsKey(sarr[1]))
-				m.put(sarr[1], 1);
-			else
-				m.replace(sarr[1], m.get(sarr[1]) + 1);
-		}
-		int c = 1;
-		for (String key : m.keySet()) {
-			// System.out.println(String.format("키 : %s, 값 : %s", key, m.get(key)));
-			c *= m.get(key) + 1;
-		}
-		return c - 1;
-	}
-
+	@SuppressWarnings("unused")
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(reader.readLine());
 
 		for (int i = 0; i < n; i++) {
 			int t = Integer.parseInt(reader.readLine());
-			System.out.println(count(t));
+			Map<String, Integer> m = new HashMap<>();
+
+			for (int j = 0; j < t; j++) {
+				StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+				String name = tokenizer.nextToken();
+				String kind = tokenizer.nextToken();
+				Integer count = m.get(kind);
+				if (count == null)
+					count = 0;
+				m.put(kind, count + 1);
+			}
+			int c = 1;
+			for (String key : m.keySet()) {
+				// System.out.println(String.format("키 : %s, 값 : %s", key, m.get(key)));
+				c *= m.get(key) + 1;
+			}
+			System.out.println(c-1);
 		}
 	}
 
