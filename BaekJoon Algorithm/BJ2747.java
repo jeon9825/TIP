68b642011ea4d38c956fa579579d4255f164d1e0
jeon9825 <jeon9825@naver.com>
2020-02-13 10:23:29 +0900

    Modify BJ2747.java

--- BaekJoon Algorithm/BJ2747.java
+++ BaekJoon Algorithm/BJ2747.java
@@ -1,18 +1,17 @@
 import java.util.Scanner;
 
 public class BJ2747 {
+	static int[] f = new int[46];
 
 	public static int Fibonacci(int n) {
-		int fibo = 0;
-
+		if (f[n] != 0)
+			return f[n];
 		if (n == 0)
-			return 0;
+			return f[0] = 0;
 		else if (n == 1)
-			return 1;
-		else {
-			fibo = Fibonacci(n - 1) + Fibonacci(n - 2);
-			return fibo;
-		}
+			return f[1] = 1;
+		else
+			return f[n] = Fibonacci(n - 1) + Fibonacci(n - 2);
 	}
 
 	public static void main(String[] args) {
