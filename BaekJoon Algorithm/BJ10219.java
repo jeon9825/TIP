ef857ae9505646cb223ab374ef9bf9059701908f
jeon9825 <jeon9825@naver.com>
2020-02-26 21:21:29 +0900

    Add BJ10219.java

---
+++ BaekJoon Algorithm/BJ10219.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ10219 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(reader.readLine());
+		for (int i = 0; i < T; i++) {
+			StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+			int H = Integer.parseInt(tokenizer.nextToken());
+			int W = Integer.parseInt(tokenizer.nextToken());
+
+			StringBuilder builder = new StringBuilder();
+			for (int j = 0; j < H; j++) {
+				builder.append(new StringBuilder(reader.readLine()).reverse());
+				builder.append("\n");
+			}
+			System.out.println(builder.toString());
+		}
+	}
+
+}
