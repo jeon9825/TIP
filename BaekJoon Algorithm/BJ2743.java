01a945895450f47978f36154de4fe13d3c3166c6
jeon9825 <jeon9825@naver.com>
2020-03-01 17:17:03 +0900

    Add BJ2743.java

---
+++ BaekJoon Algorithm/BJ2743.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BJ2743 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		String s = reader.readLine();
+		System.out.println(s.length());
+	}
+}
